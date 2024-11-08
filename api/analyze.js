47562e23b263ff75d8faf9a1703387515f1d4d9e
Emre KARDAŞ <eekardass@yahoo.com>
2024-11-08 11:12:21 +0000

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -21,6 +21,7 @@ export default async function handler(req, res) {
                 content: "Image data is required",
                 role: "error"
             }
+            
         });
         return;
     }
